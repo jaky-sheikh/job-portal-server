ca48919a6a7b2da9c6445aba551ea5960e65ff2c
jaky-sheikh <jaky@eu4m.eu>
2025-02-09 15:00:39 +0600

    conditional job api

--- index.js
+++ index.js
@@ -35,7 +35,13 @@ async function run() {
         const jobApplicationCollection = client.db('jobPortal').collection('job_application');
 
         app.get('/jobs', async (req, res) => {
-            const cursor = jobsCollection.find();
+            const email = req.query.email;
+            let query = {};
+            if (email) {
+                query = { hr_email: email }
+            }
+
+            const cursor = jobsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         });
